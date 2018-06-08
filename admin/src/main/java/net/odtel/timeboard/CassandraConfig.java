9f4ea59f295f5c27f93e28db79e8738aa0a66c36
Oleksandr Ievstratiev <oievstratiev@productengine.com>
2018-06-08 19:05:02 +0300

    added tests

--- admin/src/main/java/net/odtel/timeboard/CassandraConfig.java
+++ admin/src/main/java/net/odtel/timeboard/CassandraConfig.java
@@ -9,8 +9,10 @@ package net.odtel.timeboard;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
+@EnableCassandraRepositories
 public class CassandraConfig extends AbstractCassandraConfiguration {
     @Override
     protected String getKeyspaceName() {
--- admin/src/test/java/net/odtel/timeboard/HelloWorldConfigurationTests.java
+++ admin/src/test/java/net/odtel/timeboard/HelloWorldConfigurationTests.java
@@ -15,8 +15,9 @@
  */
 package net.odtel.timeboard;
 
-import static org.junit.Assert.*;
-
+import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
+import org.cassandraunit.spring.CassandraUnitTestExecutionListener;
+import org.cassandraunit.spring.EmbeddedCassandra;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,25 +27,80 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.context.TestExecutionListeners.MergeMode.REPLACE_DEFAULTS;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DirtiesContext
+@TestExecutionListeners(listeners = {
+        CassandraUnitDependencyInjectionTestExecutionListener.class,
+        CassandraUnitTestExecutionListener.class,
+        ServletTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class
+},
+        mergeMode = REPLACE_DEFAULTS)
+@EmbeddedCassandra(timeout = 60000)
 public class HelloWorldConfigurationTests {
+
+    private static final String TIMEPLAN_POINT = "/timeplan/";
+    private static final String TIMEPLAN_POINT_API = "/timeplan/api/";
+    private static final String HTTP_LOCALHOST = "http://localhost:";
 
     @LocalServerPort
     private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void testGreeting() throws Exception {
+    public void testGreeting() {
         ResponseEntity<String> entity = restTemplate
-                .getForEntity("http://localhost:" + this.port + "/", String.class);
+                .getForEntity(HTTP_LOCALHOST + this.port + TIMEPLAN_POINT, String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
+
+ /*   @Test
+    public void testPost() {
+        ResumePointItem resumePointItem = createResumePointItem("accountId", "deviceId");
+
+        ResponseEntity<Void> entity = restTemplate.postForEntity(HTTP_LOCALHOST + this.port + TIMEPLAN_POINT_API, resumePointItem, Void.class);
+
+        assertEquals(HttpStatus.CREATED, entity.getStatusCode());
+    }
+
+    @Test
+    public void testGet() {
+        String accountId = "accountId_" + System.currentTimeMillis();
+        String deviceId = "deviceId_" + System.currentTimeMillis();
+        ResumePointItem resumePointItem = createResumePointItem(accountId, deviceId);
+
+        ResponseEntity<Void> entity = restTemplate.postForEntity(HTTP_LOCALHOST + this.port + TIMEPLAN_POINT_API, resumePointItem, Void.class);
+
+        assertEquals(HttpStatus.CREATED, entity.getStatusCode());
+
+        ResponseEntity<ResumePointItem> forEntity = restTemplate.getForEntity(HTTP_LOCALHOST + this.port + TIMEPLAN_POINT_API + accountId + "/" + resumePointItem.getEntityId() + "/",
+                ResumePointItem.class);
+
+        assertEquals(HttpStatus.OK, forEntity.getStatusCode());
+
+        assertEquals(deviceId, forEntity.getBody().getDeviceId());
+    }
+
+    private ResumePointItem createResumePointItem(String accountId, String deviceId) {
+        UUID id = UUIDs.timeBased();
+
+        ResumePointItem resumePointItem = new ResumePointItem();
+        resumePointItem.setAccountId(accountId);
+        resumePointItem.setEntityId(id.toString());
+        resumePointItem.setDeviceId(deviceId);
+        return resumePointItem;
+    }
+*/
 
 }
